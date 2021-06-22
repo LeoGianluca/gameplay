e460c91264548901690ce8d662ce06d0697d2060
LeoGianluca <leogianluca.costa@gmail.com>
2021-06-21 22:01:24 -0300

    [DEV] Home e melhoria em SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,28 +3,26 @@ import {
 View,
 Text,
 Image, 
-StatusBar
 } from "react-native";
 
+import { styles } from "./styles";
 import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImg from '../../assets/illustration.png'
-import { styles } from "./styles";
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'} 
+          jogatinas {'\n'}
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos 
         </Text>
 
